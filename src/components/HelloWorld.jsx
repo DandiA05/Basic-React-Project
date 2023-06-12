44717b83c79fae563329eb4f77eb8e141cc3a583
DandiA05 <45591324+DandiA05@users.noreply.github.com>
2023-06-12 16:57:52 +0700

    frist push

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HelloWorld = () => {
+  return (
+    <div>
+      <p>HALLO</p>
+    </div>
+  );
+};
+
+export default HelloWorld;
---
+++ src/components/Users.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Users = () => {
+  const users = ["A", "B", "C"];
+  return <div></div>;
+};
+
+export default Users;
